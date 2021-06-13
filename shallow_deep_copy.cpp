1c5fc6163447003c77d6e9e3212cb8d5c48d7a62
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-06-13 11:09:20 -0700

    shallow copy demo

---
+++ shallow_deep_copy.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Shallow {
+	private:
+		int *data;
+	public:
+		void set_data_value(int d){ *data = d; }
+		int get_data_value(){return *data;}
+		// constructor
+		Shallow(int d);
+		// copy constructor
+		Shallow(const Shallow &source);
+		// destructor
+		~Shallow();
+}
+
+Shallow::Shallow(int d){
+	data = new int;
+	*data = d;
+}
+
+Shallow::Shallow(const Shallow &source)
+	:data(source.data){
+		cout << "Copy constructor -shallow copy" << endl;
+}
+
+Shallow::~Shallow(){
+	delete data;
+	cout << "Destructor freeing memory on heap" << endl;
+}
+
+int main(){
+
+}
