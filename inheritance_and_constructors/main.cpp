102f5263e1637b33a785a39c9a4e722fa86ccc40
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-05 16:59:14 -0700

    Add inheritance and constructor code

--- inheritance_and_constructors/main.cpp
+++ inheritance_and_constructors/main.cpp
@@ -15,16 +15,25 @@ class Base {
 
 
 class Derived:public Base {
+	// any constructors will not be inherited.
+	// so if we make an instance of this
+	
+	// using Base::Base; // this will import the non-special(copy, move stuff) constructors from the parent class Base
+
 	private:
 		int doubled_value;
 	public:
-		
+		Derived():doubled_value {0} {cout << "Derived no-args constructor invoked" << endl;}		
+		Derived(int x):doubled_value{x*2} {cout << "Derived (int) overloaded constructor invoked" << endl;}
+		~Derived(){cout << "Derived Destructor invoked" << endl;}
 };
 
 
 int main(){
 	// Base b;
-	Base b{100};
+	// Base b{100};
+	Derived d;		// no arg constructor
+	Derived {1};	// won't compile if you do not set your own overloaded constructor
 	
 	return 0;
 }
