0f263ec59fda8e1346d833355a68cf5d0ef45abb
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-12 00:31:13 -0700

    Add examples

--- Polymorphism/static_binding.cpp
+++ Polymorphism/static_binding.cpp
@@ -28,10 +28,17 @@ int main(){
 
 	// when using function greetings, the parameter is declared as Base.
 	// class instance d is a Derived class but also is a Base class.
-	// so you can pass d as a parameter, but the class method that is called
-	// will be the class method from Base. This is where polymorphism
+	// so you can pass d as a parameter, but the class method that is called or statically binded
+	// will be the class method from Base. This is where polymorphism is needed?
 	greetings(b);
 	greetings(d);
+
+
+	// Derived object is a Base object
+	// Soooo we can point to a Derived object but there will be problems
+	Base *ptr = new Derived();
+	ptr->say();
+	// ↑this will use the Base class method to print a message to the output stream
 
 	return 0;
 }
