7d11c76accba7006351ca931fb240e741d2b3648
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-15 17:14:11 -0700

    Add comments

--- smartpointers/unique.cpp
+++ smartpointers/unique.cpp
@@ -5,7 +5,18 @@
 
 int main(){
 	std::unique_ptr<int> ptr { new int {100}};
+	// for C++14, there is make_unique available.
+	// syntax goes like this
+/*
+	std::unique_ptr<int> ptr = make_unique<int>(100);
+					or
+	auto ptr = make_unique<int>(10000);
+*/
+
+
 	std::cout << *ptr << std::endl;
+
+	// pointers will be auto deleted 
 
 	return 0;
 }
