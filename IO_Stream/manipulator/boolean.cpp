4bdab5022c168718e24a1dbe9d401a0620ca9f00
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-22 00:07:25 -0700

    Add more exmaple for stream manipulator booleans

--- IO_Stream/manipulator/boolean.cpp
+++ IO_Stream/manipulator/boolean.cpp
@@ -10,5 +10,15 @@ int main(){
 	std::cout << std::boolalpha;
 	std::cout << (10 == 10) << std::endl;
 	std::cout << (1 > 2)	<< std::endl;
+
+	// Once set it will continue to display in above format
+	std::cout << (10 == 10) << std::endl;
+	std::cout << (1 > 2)	<< std::endl;
+
+	// toggle to 0/1 display
+	std::cout << std::noboolalpha;
+	std::cout << (10 == 10) << std::endl;
+	std::cout << (1 > 2)	<< std::endl;
+
 	return 0;
 }
