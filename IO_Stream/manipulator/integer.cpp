8459f3c4c73f011f0c67cf67545a0a50c3b534ca
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-22 00:32:59 -0700

    Add example for show base integer stream manipulator

--- IO_Stream/manipulator/integer.cpp
+++ IO_Stream/manipulator/integer.cpp
@@ -4,6 +4,16 @@
 int main(){
 	int num {114514};
 
+
+	// default
+	std::cout << "\nDefault integer io manipulator displays" << std::endl;
+	std::cout << std::dec << num << std::endl;
+	std::cout << std::hex << num << std::endl;
+	std::cout << std::oct << num << std::endl;
+
+	// show base format
+	std::cout << "\nShow Base format" << std::endl;
+	std::cout << std::showbase;
 	std::cout << std::dec << num << std::endl;
 	std::cout << std::hex << num << std::endl;
 	std::cout << std::oct << num << std::endl;
