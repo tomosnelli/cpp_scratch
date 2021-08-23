1a98223b580fe959d0a43d9edffc1688e376a17b
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-23 00:08:43 -0700

    Add more example for file IO

--- IO_Stream/file_stream_IO/readfile.cpp
+++ IO_Stream/file_stream_IO/readfile.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(){
 	std::fstream in_file;
+	std::string line;
+	int num;
+	std::string line2;
+	
 	in_file.open("test.txt");
 
 	if(!in_file.is_open()){
@@ -11,6 +16,9 @@ int main(){
 	} else {
 		std::cout << "Opened file test.txt" << std::endl;
 	}
+
+	in_file >> line >> num >> line2;
+	std::cout << line << num << line2 << std::endl;
 
 	in_file.close();
 
