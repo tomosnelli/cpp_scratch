6b38c12a5f48ee9b643fb53486323e43b183bd27
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-24 11:28:30 -0700

    Add example using string as output stream

--- IO_Stream/string_stream/ss.cpp
+++ IO_Stream/string_stream/ss.cpp
@@ -17,5 +17,10 @@ int main(){
 				<< std::setw(5) << num
 				<< std::setw(10) << total << std::endl;
 
+	// put the gained info into a string with ostringstream object
+	std::ostringstream oss {};
+	oss << name << " " << num << " " << total;
+	std::cout << oss.str() << std::endl;
+
 	return 0;
 }
