9409bdc02de357c0e39de065153cfece5cce31c6
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-25 15:10:09 -0700

    Add more example for function templates

--- Stantard_Template_Library/function_template.cpp
+++ Stantard_Template_Library/function_template.cpp
@@ -11,7 +11,21 @@ void func(T1 a, T2 b){
 	std::cout << a << " " << b << std::endl;
 }
 
+struct Person {
+	std::string name;
+	int age;
+	bool operator<(const Person &rhs) const{
+		return this->age < rhs.age;
+	}
+};
+
 int main(){
+	Person p1 {"Curly", 40};
+	Person p2 {"Moe", 30};
+
+	Person p3 = min(p1, p2);
+	std::cout << p3.name << " is younger" << std::endl;
+
 	std::cout << min<int>(2, 3) << std::endl;
 	std::cout << min(2, 3) << std::endl;
 	std::cout << min('A', 'B') << std::endl;
