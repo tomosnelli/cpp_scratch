faad9222a156a65870b70e42f68d34e841d95419
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-30 16:53:29 -0700

    Add example for containers and iterators

--- Stantard_Template_Library/iterator.cpp
+++ Stantard_Template_Library/iterator.cpp
@@ -53,15 +53,62 @@ void test2(){
 }
 
 void test3(){
+	std::vector<int> num {1, 2, 3, 4, 5};
+	std::vector<int>::const_iterator iter = num.begin();
+	// auto iter = num.cbegin() is equilvalent to the line above
 
+	while(iter != num.end()){
+		std::cout << *iter << std::endl;
+		iter++;
+	}
+
+	iter = num.begin();
+	while(iter != num.end()){
+		// *iter = 0		this gives a compiler error
+		iter++;
+	}
+}
+
+void test4(){
+
+	// iterator over vector
+	std::vector<int> vect {1,2,3,4,5};
+	auto iter = vect.rbegin();			// reverse iterator over vector of ints, start at 5
+	std::cout << *iter << std::endl;
+	while(iter != vect.rend()){
+		std::cout << *iter << std::endl;
+		iter++;
+	}
+
+	// iterator over list
+	std::list<std::string> name {"Tomo", "Kaneko", "Snelling"};
+	auto iter1 = name.crbegin(); // iterator over list of strings point to Snelling
+	std::cout << *iter1 << std::endl;
+	iter1++;
+	std::cout << *iter1 << std::endl;
+
+	// iterator over map, a python dictonary equivalent
+	std::map<std::string, std::string> favorites {
+		{"raindrops", "roses"},
+		{"whiskers", "kittens"},
+		{"Bright copper", "Kettles"}
+	};
+	auto iter2 = favorites.begin();
+	while(iter2 != favorites.end()){
+		std::cout << iter2->first << " : " << iter2->second << std::endl;
+		iter2++;
+	}
+}
+
+void test5(){
+	
 }
 
 int main(){
 	// test1();
-	test2();
 	// test2();
 	// test3();
-	// test4();
+	test4();
 	// test5();
 	return 0;
 }
