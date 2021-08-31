35854b41aeaf9cfb295ead4d419916258d9c2910
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-30 20:23:32 -0700

    Add example of std::count algorithm

--- Stantard_Template_Library/algorithm_demo.cpp
+++ Stantard_Template_Library/algorithm_demo.cpp
@@ -45,7 +45,14 @@ void find_test(){
 	}
 }
 
+void count_test(){
+	std::vector<int> vec {1,2,3,4,5,1,1,1};
+	int num = std::count(vec.begin(), vec.end(), 1);
+	std::cout << num << " occurrences of 1 found" << std::endl;
+}
+
 int main(){
 	find_test();
+	count_test();
 	return 0;
 }
